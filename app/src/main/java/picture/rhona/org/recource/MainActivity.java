65044cf403ff7da7118f8dc6ee74e4faa5d669bb
Mozator <ym199210@163.com>
2018-05-14 13:41:48 +0800

    dd

---
+++ app/src/main/java/picture/rhona/org/recource/MainActivity.java
@@ -0,0 +1,13 @@
+package picture.rhona.org.recource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
